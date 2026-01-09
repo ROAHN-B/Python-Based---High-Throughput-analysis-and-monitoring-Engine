3b1b954e5eead83f422c763387e2060460b05c9f
Rohan Belsare <rohanbelsare113@gmail.com>
2026-01-09 19:03:07 +0530

    function for sending static mails

--- config/email_config.py
+++ config/email_config.py
@@ -1,18 +1,38 @@
 import smtplib
 from email.mime.text import MIMEText
 
-sender = "rohanbelsare113@gmail.com"
-receiver = "rohanbelsare113@gmail.com"
-password = "auex mhqe squb szvs"
+# sender = "rohanbelsare113@gmail.com"
+# receiver = "rohanbelsare113@gmail.com"
+# password = "auex mhqe squb szvs"
 
-message = MIMEText("Sending mail using python")
+# message = MIMEText("Sending mail using python")
 
-message["subject"] = "test Email"
-message["From"] = sender
-message["To"] = receiver
+# message["subject"] = "test Email"
+# message["From"] = sender
+# message["To"] = receiver
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()  # for encryption
-server.login(sender, password)
-server.sendmail(sender, receiver, message.as_string())
-server.quit()
+# server = smtplib.SMTP("smtp.gmail.com", 587)
+# server.starttls()  # for encryption
+# server.login(sender, password)
+# server.sendmail(sender, receiver, message.as_string())
+# server.quit()
+
+
+def sending_mail(sender, receiver, subject):
+    message = MIMEText("sending mail using python")
+    message["subject"] = subject
+    message["From"] = sender
+    message["To"] = receiver
+    password = "auex mhqe squb szvs"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message.as_string())
+    server.quit()
+
+
+sending_mail(
+    "rohanbelsare113@gmail.com",
+    "rohanbelsare113@gmail.com",
+    "sending mail using python",
+)
