92e9aae0e0bfb432c58d5a290ea0b16500e94281
Rohan Belsare <rohanbelsare113@gmail.com>
2026-01-27 18:48:24 +0530

    NC

--- backend/router/service.py
+++ backend/router/service.py
@@ -3,6 +3,7 @@ from mysql.connector import Error
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+
 class UserCreate(BaseModel):
     email: str
     password: str
@@ -10,51 +11,53 @@ class UserCreate(BaseModel):
 
 router = APIRouter()
 
-@router.post("/create/user") #API end point
-def create_user(User:UserCreate):
+
+@router.post("/create/user")  # API end point
+def create_user(User: UserCreate):
     try:
-        db=connect_sql()
+        db = connect_sql()
         cursor = db.cursor(dictionary=True)
-        query = "insert into credentials (name,age,email) values(%s,%s)"
-        cursor.execute(query,(User.password,User.email))
+        query = "insert into credentials (password,email) values(%s,%s)"
+        cursor.execute(query, (User.password, User.email))
         db.commit()
         cursor.close()
         db.close()
-        return {"Message":"User added to database succesfully"}
+        return {"Message": "User added to database succesfully"}
     except Error as e:
         print("Error : ", e)
 
 
 @router.get("/get/user/info")
 def get_user(id):
-    db=connect_sql()
-    cursor=db.cursor(dictionary=True)
-    query="select name,age,email from credentials where id = %s "
-    cursor.execute(query,(id,))
-    user=cursor.fetchone()
+    db = connect_sql()
+    cursor = db.cursor(dictionary=True)
+    query = "select password,email from credentials where id = %s "
+    cursor.execute(query, (id,))
+    user = cursor.fetchone()
     db.close()
     cursor.close()
-    return {"message":"USer found!!","data":user}
+    return {"message": "USer found!!", "data": user}
+
 
 @router.put("/update/user")
-def update_user(new_user:create_user):
-    db=connect_sql()
-    cursor=db.cursor(dictionary=True)
-    query="update credentials set name=%s,age=%s,email=%s where id=%s"
-    cursor.execute(query,(new_user.email,new_user.password))
+def update_user(new_user: create_user):
+    db = connect_sql()
+    cursor = db.cursor(dictionary=True)
+    query = "update credentials set password=%s,email=%s where id=%s"
+    cursor.execute(query, (new_user.password, new_user.email))
     db.commit()
     db.close()
     cursor.close()
-    return {"Message":"User updated seccesfully!!"}
+    return {"Message": "User updated seccesfully!!"}
+
 
 @router.delete("/delete/user/info")
 def delete_user(id):
-    db=connect_sql()
-    cursor=db.cursor(dictionary=True)
-    query="delete from  credentials where id=%s"
-    cursor.execute(query,(id,))
+    db = connect_sql()
+    cursor = db.cursor(dictionary=True)
+    query = "delete from  credentials where id=%s"
+    cursor.execute(query, (id,))
     db.commit()
     cursor.close()
     db.close()
-    return {"message":"user deleted succesfully!!"}
-
+    return {"message": "user deleted succesfully!!"}
--- main.py
+++ main.py
@@ -8,8 +8,25 @@
 # from config.email_config import send_mail
 from fastapi import FastAPI
 from backend.router import service
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins=[     #Need to add origin so that frontend can send method to backend.
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, ## Allow connection to database.
+    allow_methods=["*"], ##allow all methods like POST,GET,PUT,DELETD,etc.
+    allow_headers=["*"],
+)
+
 
 # user_mail = "rohanbelsare113@gmail.com"
 # def main():
